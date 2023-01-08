58d2b9a014670e2b20192d1a3460c11cce9ef869
Ali Maher <ali.maher.mohammed@gmail.com>
2023-01-08 09:06:50 +0200

    Finished Product List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
+import { useState } from "react"
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Cart from "./Components/Cart"
+import ProductList from "./Components/ProductList"
 
-    </div>
+function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
   )
+
+  return content
 }
 
 export default App
--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -18,7 +18,7 @@ const REDUCER_ACTION_TYPE = {
   SUBMIT: "SUBMIT"
 }
 
-export type RducerActionType = typeof REDUCER_ACTION_TYPE
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE
 
 export type ReducerAction = {
   type: string,
